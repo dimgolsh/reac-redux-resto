87c92d4c279d6658b2465d8676171bd41e1b7c25
dimon <dimagol64@gmail.com>
2020-04-20 23:17:22 +0300

    Updates

---
+++ precache-manifest.524ebb55844c342c8b9bfafc1ad43eb5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a6dc9817c93cc7ebb5691c0d24be79c",
+    "url": "/react-redux-resto/index.html"
+  },
+  {
+    "revision": "95a947966763f8732ce3",
+    "url": "/react-redux-resto/static/css/main.b6fb1074.chunk.css"
+  },
+  {
+    "revision": "fb2ba13791f11d5c6859",
+    "url": "/react-redux-resto/static/js/2.b32d15af.chunk.js"
+  },
+  {
+    "revision": "23900771a863482f56e68d68cf1f3072",
+    "url": "/react-redux-resto/static/js/2.b32d15af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95a947966763f8732ce3",
+    "url": "/react-redux-resto/static/js/main.0f22ec61.chunk.js"
+  },
+  {
+    "revision": "03835ec1be78566f6fc1",
+    "url": "/react-redux-resto/static/js/runtime-main.f09de9ed.js"
+  },
+  {
+    "revision": "698e5e807daf0026f6ac590ce288822c",
+    "url": "/react-redux-resto/static/media/food-bg.698e5e80.jpg"
+  },
+  {
+    "revision": "701d6d1d57f5d528c4db1e887ff29e82",
+    "url": "/react-redux-resto/static/media/shopping-cart-solid.701d6d1d.svg"
+  }
+]);
